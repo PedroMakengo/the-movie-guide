c89aa67b64c0010ef3494eca6a42361590f00543
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-26 10:21:20 -0300

    Trabalhando com rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 
+import logo from "../../assets/logo/logo.svg";
+
 function Header() {
   return (
     <header>
+      <div>
+        <img src={logo} alt="" />
+      </div>
       <nav>
         <Link to="/">Movies</Link>
         <Link to="/popular-persons">People</Link>
         <Link to="/profile">Profile</Link>
       </nav>
+      <input type="text" placeholder="Search a title" />
     </header>
   );
 }
