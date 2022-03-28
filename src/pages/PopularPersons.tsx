b338a07a59ffcc395280561d51ff9aa9ca036beb
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-28 14:08:36 -0300

    Trabalhando nas páginas populares

--- src/pages/PopularPersons.tsx
+++ src/pages/PopularPersons.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
+import Head from "../components/Head";
 
 function PopularPersons() {
   return (
-    <div>
+    <main>
+      <Head title="PersonDetails" description="Testando" />
       <h1>PopularPersons</h1>
-
       <Link to="/person-details/1">Pedro Makengo</Link>
       <Link to="/person-details/2">Antónia Manuel Vicente</Link>
       <Link to="/person-details/3">Dorcas Kifi</Link>
       <Link to="/person-details/4">Abel Makengo</Link>
       <Link to="/person-details/5">Isabel Ntanda</Link>
-    </div>
+    </main>
   );
 }
 
