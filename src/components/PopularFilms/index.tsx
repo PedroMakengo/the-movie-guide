31e2dba44eadd3f451e0ed5bbcbc34bed96c3b46
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-31 11:17:20 -0300

    Trabalhando com api

--- src/components/PopularFilms/index.tsx
+++ src/components/PopularFilms/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   ContainerPopular,
   ListFilms,
@@ -7,11 +8,114 @@ import {
 } from "./styles";
 
 function PopularFilms() {
+  const [movie, setMovie] = useState<[] | true>(true);
+  const [loading, setLoading] = useState(false);
+
+  if (movie === null) return null;
+  if (loading) return <p>Processando</p>;
   return (
     <ContainerPopular>
       <h2>Popular Films This Week</h2>
 
       <ListFilms>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
+        <Movie>
+          <BannerMovie />
+          <Content>
+            <h3>Filme 1</h3>
+            <span>Trabalhando</span>
+          </Content>
+        </Movie>
         <Movie>
           <BannerMovie />
           <Content>
