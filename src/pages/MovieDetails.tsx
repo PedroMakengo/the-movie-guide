0b16fd65810d277b9abfd2808d1ecbc9bda0bbab
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-31 13:53:51 -0300

    Trabalhando nas alterações

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Head from "../components/Head";
 
+const API_KEY = "a90d2e092e8721ef7b2eb3a1385a0df6";
+const API_BASE = "https://api.themoviedb.org/3";
+
 function MovieDetails() {
   const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `${API_BASE}/trending/all/week?language=pt-BR&api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+    })();
+  }, [id]);
 
   return (
     <main>
